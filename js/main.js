c2f38566c5de212f7aaa151ab776e3dd82f505a8
Kevin Ochanda <kevohyout44@gmail.com>
2020-05-16 16:14:12 +0300

    second commit
    I've continued working on the project. I added a few sections and now i'm at the footer.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Select element function
+
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function() {
+    body.classList.toggle('open');
+});
